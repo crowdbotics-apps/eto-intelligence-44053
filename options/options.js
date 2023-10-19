e40727f03151900902a17796d5ddd7acabb16a97
Crowdbotics <team@crowdbotics.com>
2023-10-19 23:50:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "eto-intelligence-44053",
+  url: "https://eto-intelligence-44053.botics.co",
+  api: "https://eto-intelligence-44053.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
